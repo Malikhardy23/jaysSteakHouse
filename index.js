0eb911334ccfe648078f947b64b71e2310b5cc32
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2021-02-22 23:21:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // variables 
 
 const menuBtn = document.getElementById("mobileMenuBtn");
-const mobileMenu = document.getElementById("mobileMenu");
+const mobileMenu = document.getElementsByClassName('mobileMenu');
 const mobileMenuLinks = [
     document.getElementsByTagName("a")[5],
     document.getElementsByTagName("a")[6],
@@ -9,23 +9,10 @@ const mobileMenuLinks = [
     document.getElementsByTagName("a")[8],
     document.getElementsByTagName("a")[9]
 ];
-console.log(mobileMenuLinks);
 
 
-//const mobileMenuKids = mobileMenu.children;
-//console.log(mobileMenuKids);
-
-/* menuBtn.addEventListener('click', () => {
-    if(mobileMenu.style.display == "flex"){
-        mobileMenu.style.display = "none";
-    } else {
-        //mobileMenu.style.padding = "50px";
-        mobileMenu.style.display = "flex";
-    }
-}) */
-
-const toggleMenu = () => {
-    mobileMenu.classList.toggle('active');
+let clickedBtn = () => {
+    mobileMenu[0].classList.toggle('active');
 }
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('click', clickedBtn);
